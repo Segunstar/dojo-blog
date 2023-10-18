d430770b9a10b5bc24c538f4eab2c90cbaa6f17c
Star boy <ekundayoisegun@gmail.com>
2023-10-18 20:39:11 +0100

    true and through with the dojo blog

--- src/Home.js
+++ src/Home.js
@@ -1,26 +1,17 @@
-import { useState } from 'react';
+import useFetch from './useFetch';
+import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-    {title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1},
-    {title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2},
-    {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3},
-
-
-    ])
-   
-
+   const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs')
 
     return ( 
        <div className="home">
-       {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-        <h2>{blog.title}</h2>
-        <p>Written by {blog.author}</p>
-        </div>
-       ))}
+        {error && <div>{ error }</div>}
+        {isPending && <div>Loading...</div>}
+       { blogs && <BlogList blogs={blogs} title='All Blogs' />}
+       {/* <BlogList blogs={blogs.filter((blog) => blog. author === 'mario')} title="Mario's Blogs"/> */}
        </div> 
      );
 }
  
-export default Home;
+export default Home; 
